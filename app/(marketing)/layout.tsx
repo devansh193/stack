d67d80b0bb72cb59fce3a87a64683b7cf3c058e5
devansh <devanshverma193@gmail.com>
2024-08-25 00:16:56 +0530

    init

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -3,9 +3,9 @@ import { Navbar } from "./_components/Navbar";
 
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="h-full bg-zinc-100">
-        <Navbar />
-      <main className=" pt-40 pb-80 bg-zinc-50">{children}</main>
+    <div className="min-h-screen bg-zinc-100">
+      <Navbar />
+      <main className=" pt-40 pb-20">{children}</main>
       <Footer />
     </div>
   );
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+  name: "StackTrack",
+  description: "StackTrack is a task management tool that helps you manage your tasks efficiently.",
+};
