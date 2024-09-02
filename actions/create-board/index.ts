e312da49ae32bc6c7a8d85ca460f8bd8ff65dc75
devansh <devanshverma193@gmail.com>
2024-09-02 19:26:13 +0530

    create-board

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -8,19 +8,38 @@ import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateBoard } from "./schema";
 
 const handle = async (data: InputType): Promise<ReturnType> => {
-  const { userId } = auth();
-  if (!userId) {
+  const { userId, orgId } = auth();
+  if (!userId || !orgId) {
     return {
       error: "Unauthorized",
     };
   }
-  const { title } = data;
+  const { title, image } = data;
+  const [
+ imageId, imageThumbUrl, imageFullUrl, imageLinkHtml, imageUserName 
+  ] = image.split("|");
+  console.log({
+    imageId, imageThumbUrl, imageFullUrl, imageLinkHtml, imageUserName 
+  });
+
+  if(!imageId || !imageThumbUrl || !imageFullUrl || !imageLinkHtml || !imageUserName ){
+    return {
+      error:"Missing fields. Failed to create board."
+    };
+  }
+
   let board;
 
   try {
     board = await db.board.create({
       data: {
         title,
+        orgId,
+        imageId,
+        imageThumbUrl,
+        imageFullUrl,
+        imageUserName,
+        imageLinkHtml,
       },
     });
   } catch (error) {
--- actions/create-board/schema.ts
+++ actions/create-board/schema.ts
@@ -7,4 +7,8 @@
     }).min(3,{
         message:"Title is too short."
     }),
+    image: z.string({
+        required_error: "Image is required.",
+        invalid_type_error:"Image is required",
+    }),
  });
