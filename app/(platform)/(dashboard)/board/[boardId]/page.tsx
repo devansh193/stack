733e21e1c1f560dc09f84288c53c85b94cc94f52
devansh <devanshverma193@gmail.com>
2024-09-05 15:03:24 +0530

    board-page-actions

--- app/(platform)/(dashboard)/board/[boardId]/page.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -1,7 +1,21 @@
-const BoardIdPage = () =>{
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+interface BoardIdPageProps {
+    params:{
+        boardId: string
+    };
+};
+
+const BoardIdPage = async ({params}:BoardIdPageProps) =>{
+    const {orgId} = auth();
+    if(!orgId){
+        redirect("/select-org");
+    }
     return(
         <div>
-            BoardId
+            
         </div>
     );
 };
