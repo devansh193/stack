2da8c02fe894dd039a5399dfcd19638f418d18e4
devansh <devanshverma193@gmail.com>
2024-09-08 00:20:53 +0530

    list-create

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-container.tsx
@@ -2,18 +2,33 @@
 
 import { List } from "@prisma/client";
 import { ListForm } from "./list-form";
+import { useEffect, useState } from "react";
+import { ListItem } from "./list-item";
 
 interface ListContainerData {
-    data: List[];
-    boardId: string;
-};
-
-export const ListContainer = ({boardId, data}:ListContainerData)=>{
-    return(
-        <ol>
-            <ListForm/>
-            <div className="flex shrink-0 w-1"/>
-        </ol>
-    )
+  data: List[];
+  boardId: string;
 }
 
+export const ListContainer = ({ boardId, data }: ListContainerData) => {
+  const [orderedData, setOrderedData] = useState(data);
+  useEffect(() => {
+    setOrderedData(data);
+  }, [data]);
+
+  return (
+    <ol className="flex gap-x-3 h-full">
+        {orderedData.map((list, index)=>{
+            return (
+                <ListItem
+                key={list.id}
+                index={index}
+                data={list}
+                />
+            )
+        })}
+      <ListForm />
+      <div className="flex shrink-0 w-1" />
+    </ol>
+  );
+};
